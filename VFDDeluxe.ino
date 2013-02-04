837d215662d0e9b798369c3266055b0f51478190
Per Johan Groland <per@akafugu.jp>
2013-02-04 19:03:11 +0900

    Cleanup

--- VFDDeluxe.ino
+++ VFDDeluxe.ino
@@ -292,70 +292,10 @@ void read_humidity()
     show_humidity(96);    
 }
 
-/*
-#include "blacklist.h"
-
-bool binary_search(const char *key, int imin, int imax){
-  int pos;
-  int cond=0;
-  
-  char buf[5];
-
-  while (imin<=imax){
-    pos = (imin+imax)/2;
-    
-    strcpy_P(buf, (char*)pgm_read_word(&(flw_blacklist[pos])));
-    cond = strcmp(key, buf);
-    
-    if (cond == 0)
-      return true;
-    else if (cond>0)
-      imin=pos+1;
-    else
-      imax=pos-1;
-  }
-  return false;
-}
-
-char* check_blacklist()
-{
-  char* w = flw.get_word();
-  
-  // assume a maximum of 5 censored words chosen in a row
-  for (uint8_t i = 0; i < 5; i++) {
-    if (binary_search(w, 0, BLACKLIST_SIZE)) { // censored
-      Serial.print("blacklisted: ");
-      Serial.println(w);
-      w = flw.get_word();
-    }
-  }
-  
-  return w;
-}
-*/
-
 void read_flw()
 {
 #ifdef HAVE_FLW
-
-/*
-  char* foo = flw.get_word();
-  Serial.println(foo);
-  
-    if (binary_search(foo, 0, BLACKLIST_SIZE))
-      Serial.println("blacklisted!");
-  */
-  
-  //bool blacklisted = flw.check_blacklist(foo);
-  //if (blacklisted) Serial.println("blacklisted!");
-  
-  //char* foo = check_blacklist();
-
-  char* foo = flw.get_word();
-  Serial.println(foo);
-  set_string(foo);
-
-  //set_string(flw.get_word());
+    set_string(flw.get_word());
 #endif
 }
 
--- flw_blacklist.h
+++ flw_blacklist.h
@@ -24,10 +24,8 @@
 
 #include <avr/pgmspace.h>
 
-  prog_char word_t0[] PROGMEM = "NORY";
 prog_char word_0[]  PROGMEM = "NAHF";
 prog_char word_1[]  PROGMEM = "NEFR";
-  prog_char word_t1[] PROGMEM = "ONYY"; // test
 prog_char word_2[]  PROGMEM = "PNJX";
 prog_char word_3[]  PROGMEM = "PYVG";
 prog_char word_4[]  PROGMEM = "PBPX";
@@ -50,33 +48,28 @@ prog_char word_20[] PROGMEM = "WVMM";
 prog_char word_21[] PROGMEM = "XVXR";
 prog_char word_22[] PROGMEM = "ZHSS";
 prog_char word_23[] PROGMEM = "ANMV";
-  prog_char word_t2[] PROGMEM = "AVPR"; // test
 prog_char word_24[] PROGMEM = "CNXV";
 prog_char word_25[] PROGMEM = "CVFF";
-  prog_char word_t3[] PROGMEM = "CYNL"; // test
 prog_char word_26[] PROGMEM = "CBBA";
 prog_char word_27[] PROGMEM = "CBBC";
 prog_char word_28[] PROGMEM = "CBEA";
 prog_char word_29[] PROGMEM = "ENCR";
-  prog_char word_t4[] PROGMEM = "DHVM"; // test
 prog_char word_30[] PROGMEM = "FUVG";
 prog_char word_31[] PROGMEM = "FZHG";
 prog_char word_32[] PROGMEM = "FCVP";
 prog_char word_33[] PROGMEM = "FYHG";
-  prog_char word_t5[] PROGMEM = "test"; // test  
 prog_char word_34[] PROGMEM = "GVGF";
-  prog_char word_t6[] PROGMEM = "GHOR"; // test
 prog_char word_35[] PROGMEM = "GHEQ";
 prog_char word_36[] PROGMEM = "GJNG";
 prog_char word_37[] PROGMEM = "JNAX";
 
-#define BLACKLIST_SIZE 38+7
+#define BLACKLIST_SIZE 37
 
 PROGMEM const char *flw_blacklist[] =
 {   
-	word_t0, word_0, word_1, word_t1, word_2, word_3, word_4, word_5, word_6, word_7, word_8, word_9,
+	word_0, word_1, word_2, word_3, word_4, word_5, word_6, word_7, word_8, word_9, 
 	word_10, word_11, word_12, word_13, word_14, word_15, word_16, word_17, word_18, word_19,
-	word_20, word_21, word_22, word_23, word_t2, word_24, word_25, word_t3, word_26, word_27, word_28, word_29, word_t4, 
-	word_30, word_31, word_t5, word_32, word_33, word_t5, word_34, word_t6, word_35, word_36, word_37
+	word_20, word_21, word_22, word_23, word_24, word_25, word_26, word_27, word_28, word_29, 
+	word_30, word_31, word_32, word_33, word_34, word_35, word_36, word_37
 };
 
